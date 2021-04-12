94e2392d280fd914643c933457c516b794cc90ee
medium0fry <medium0fry@gmail.com>
2021-04-12 08:56:14 -0400

    Finished day 25

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/js/main.js
@@ -0,0 +1,18 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e) {
+   console.log(this.classList.value);
+   e.stopPropagation(); // Prevents bubbling to parents
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+   capture: true, // Runs the function from parent -> child; false by default
+   once: true // Listens for a click once and then unbinds itself
+}));
+
+button.addEventListener('click', () => {
+   console.log('Click!!!');
+}, {
+   once: true
+})
