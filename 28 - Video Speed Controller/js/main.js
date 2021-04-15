35604236c21220b5c86f1a4ca5f74edf5b446ca5
medium0fry <medium0fry@gmail.com>
2021-04-15 09:41:29 -0400

    Finished day 28

---
+++ 28 - Video Speed Controller/js/main.js
@@ -0,0 +1,26 @@
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+
+function handleMove(e) {
+   const y = e.pageY - this.offsetTop;
+   const percent = y / this.offsetHeight;
+   const height = Math.round(percent * 100) + '%';
+
+   const min = 0.4;
+   const max = 4;
+   const playbackRate = percent * (max - min) + min;
+   setVisuals(height, playbackRate);
+
+}
+
+function setVisuals(height, playbackRate) {
+   bar.style.height = height;
+   bar.textContent = `${playbackRate.toFixed(2)}x`;
+
+   video.playbackRate = playbackRate;
+}
+
+
+speed.addEventListener('mousemove', handleMove);
